fae4893dd4fcc3918d25239dda439bda28cfccfc
isavishal <108510573+isavishal@users.noreply.github.com>
2022-07-19 22:23:18 +0400

    Update settings.py

--- myshop/settings.py
+++ myshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["https://myshopnew.herokuapp.com","myshopnew.herokuapp.com"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
